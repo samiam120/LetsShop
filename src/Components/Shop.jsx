c2a4449c2ad3e2b557f5e5edcafde3eccd06b69a
samiam120 <samassefa3@gmail.com>
2025-06-10 13:19:11 -0700

    Shop component functionality - error, loading state - display items on mount

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from 'react';
+import ItemCard from './ItemCard';
+
 const Shop = () => {
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error('Server error');
+        }
+        return response.json();
+      })
+      .then((data) => setProducts(data))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) return <p>Loading</p>;
+  if (error) return <p>A network error was encountered</p>;
   return (
     <div>
       <h1>Shop</h1>
+      {products.map((product) => {
+        return (
+          <div key={product.id}>
+            <ItemCard item={product} />
+          </div>
+        );
+      })}
     </div>
   );
 };
