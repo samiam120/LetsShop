d284ee1c5c8a60d8eddfb6b3a36f9df963f897bf
samiam120 <samassefa3@gmail.com>
2025-06-10 13:14:12 -0700

    rendering logic

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,8 +1,24 @@
+import { useContext } from 'react';
+import { OutletContext } from '../App';
+import CartItemCard from './CartItemCard';
 const Cart = () => {
-    return (
-        <div>
-            <h1>Cart</h1>
-        </div>
-    )
-}
+  const { cartState } = useContext(OutletContext);
+
+  if (cartState.cart.length == 0)
+    return <p>Your Cart is Empty, go shop and add some items!</p>;
+
+  return (
+    <div>
+      {cartState.cart.map((item) => {
+        return (
+          <div key={item.id}>
+            <CartItemCard item={item} />
+          </div>
+        );
+      })}
+      <hr />
+      <h2>Total: {cartState.totalPrice.toFixed(2)}</h2>
+    </div>
+  );
+};
 export default Cart;
