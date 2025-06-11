049e70b5bd4127d4f20440453aad5a3384a37cf3
samiam120 <samassefa3@gmail.com>
2025-06-11 13:41:19 -0700

    Styles/Layout for ItemCard

--- src/Components/ItemCard.jsx
+++ src/Components/ItemCard.jsx
@@ -1,23 +1,42 @@
+import styles from '../styles/itemcard.module.css';
 import { useContext, useState } from 'react';
 import { OutletContext } from '../App';
 
 const ItemCard = ({ item }) => {
   const [quantity, setQuantity] = useState(1);
   const { addToCart } = useContext(OutletContext);
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
-    <div>
-      <div className="img-container">
-        <img src={item.image} alt={item.title} />
+    <div className={styles.card}>
+      <div>
+        {<img src={item.image} alt={item.title} className={styles.image} />}
       </div>
-      <h1>{item.title}</h1>
-      <p>{item.price}</p>
-      <button onClick={() => setQuantity((prevQuantity) => prevQuantity + 1)}>
-        Quantity: {quantity}
-      </button>
-      <button onClick={() => addToCart({ ...item, quantity })}>
-        Add to Cart
-      </button>
+      <h1 className={styles.cardTitle}>{item.title}</h1>
+      <p className={styles.price}>{item.price}</p>
+
+      <div className={styles.buttons}>
+        <button
+          class={styles.quantityButton}
+          disabled={isLoading}
+          onClick={() => setQuantity((prevQuantity) => prevQuantity + 1)}
+        >
+          Quantity: {quantity}
+        </button>
+        <button
+          class={styles.addToCartBtn}
+          disabled={isLoading}
+          onClick={() => {
+            setIsLoading(true);
+            addToCart({ ...item, quantity });
+            setTimeout(() => {
+              setIsLoading(false);
+            }, 2000);
+          }}
+        >
+          {isLoading ? <span className={styles.spinner}></span> : 'Add to Cart'}
+        </button>
+      </div>
     </div>
   );
 };
