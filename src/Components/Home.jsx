eda73aae40900f210db2589d0214b4937e77d3f7
samiam120 <samassefa3@gmail.com>
2025-06-10 17:18:32 -0700

    link added to home page and homepage styles

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,17 @@
-import NavBar from "./NavBar";
+import { Link } from 'react-router-dom';
+import styles from '../styles/home.module.css';
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
-}
+  return (
+    <div className={styles.home}>
+      <h1 className={styles.heading}>LetsShop 🛍️</h1>
+      <p className={styles.subtext}>
+        Discover the best deals on your favorite produtcts
+      </p>
+      <Link to="/shop">
+        <button className={styles.ctaBtn}>Shop Now</button>
+      </Link>
+    </div>
+  );
+};
 
 export default Home;
