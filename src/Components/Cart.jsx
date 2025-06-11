fc97d169da817f9fd537f347c4c0bc5e44fa68fe
samiam120 <samassefa3@gmail.com>
2025-06-11 15:32:30 -0700

    Cart + checkout changes - added styles to cart item cards - styles to cart

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,24 +1,32 @@
 import { useContext } from 'react';
 import { OutletContext } from '../App';
 import CartItemCard from './CartItemCard';
+import styles from '../styles/cart.module.css'
 const Cart = () => {
   const { cartState } = useContext(OutletContext);
 
   if (cartState.cart.length == 0)
-    return <p>Your Cart is Empty, go shop and add some items!</p>;
+    return <p className={styles.empty}>Your Cart is Empty, go shop and add some items!</p>;
 
   return (
-    <div>
-      {cartState.cart.map((item) => {
-        return (
-          <div key={item.id}>
-            <CartItemCard item={item} />
-          </div>
-        );
-      })}
-      <hr />
-      <h2>Total: {cartState.totalPrice.toFixed(2)}</h2>
+    <>
+    {cartState.cart.length == 0 && <p className={styles.empty}>Your Cart is Empty, go shop and add some items!</p>}
+    <div className={styles.cartContainer}>
+      <div className={styles.cartItems}>
+        {cartState.cart.map((item) => {
+          return (
+            <div key={item.id}>
+              <CartItemCard item={item} />
+            </div>
+          );
+        })}
+      </div>
+      <div className={styles.checkout}>
+        <h2>Total: {cartState.totalPrice.toFixed(2)}</h2>
+        <button>Proceed to Checkout</button>
+      </div>
     </div>
+    </>
   );
 };
 export default Cart;
--- src/Components/CartItemCard.jsx
+++ src/Components/CartItemCard.jsx
@@ -1,17 +1,22 @@
 import { useContext } from 'react';
 import { OutletContext } from '../App';
+import styles from '../styles/cartitemcard.module.css';
 
 const CartItemCard = ({ item }) => {
   const { removeFromCart } = useContext(OutletContext);
 
   return (
-    <div>
-      <img src={item.image} alt={item.title} />
-      <h2>{item.title}</h2>
-      <p>Price: ${item.price}</p>
-      <p>Quantity: {item.quantity}</p>
-      <p>Total: ${item.price * item.quantity}</p>
-      <button onClick={() => removeFromCart(item.id)}>Remvove From Cart</button>
+    <div className={styles.cardContainer}>
+      <div className={styles.imageContainer}>
+        <img className={styles.image} src={item.image} alt={item.title} />
+      </div>
+      <div className={styles.card}>
+        <h2>{item.title}</h2>
+        <p className={styles.price}>Price: ${item.price}</p>
+        <p clasName={styles.quantity}>Quantity: {item.quantity}</p>
+        <p className={styles.total}>Total: ${item.price * item.quantity}</p>
+        <button className={styles.removeButton} onClick={() => removeFromCart(item.id)}>Remvove From Cart</button>
+      </div>
     </div>
   );
 };
